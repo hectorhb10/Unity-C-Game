7e7a083c870dc00f957e49a8adbae4c89f7df7d4
Héctor U. Becerra <49003537+hectorhb10@users.noreply.github.com>
2021-01-12 17:15:47 -0600

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public GameObject gamePlayCanvas;
+    public GameObject gameScoreCanvas;
+    public GameObject player;
+    
+    public void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void Play()
+    {
+        gamePlayCanvas.SetActive(false);
+        gameScoreCanvas.SetActive(true);
+        player.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        gameScoreCanvas.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("GameReplay");
+    }
+}
